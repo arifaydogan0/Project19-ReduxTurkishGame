715e040ffb1d01ccf08301db692852ee3898ace2
Arif Aydoğan <aydoganarif@yahoo.com>
2022-07-19 23:22:28 +0300

    aa

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import GameArea from "./components/GameArea";
+import "./scss/App.scss"
+
+function App() {
+  return (
+    <div className="App">
+      <GameArea/>
+    </div>
+  );
+}
+
+export default App;
